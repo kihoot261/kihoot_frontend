56ee4462c414fab7ccdb1fb14ae405bc6ebc2f5f
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2026-01-25 20:41:21 +0100

    some navigation added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import './App.css';
 import Quiz from './pages/Quiz';
 import Info from './pages/Info';
 import Profile from './pages/Profile';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
+  const location = useLocation();
+  const showOnlyOnHome = location.pathname === '/';
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/info' element={<Info></Info>}></Route>
-          <Route path='/quiz' element={<Quiz></Quiz>}></Route>
-          <Route path='/profile' element={<Profile></Profile>}></Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+
+      {
+        showOnlyOnHome &&
+        <>
+          <Navbar></Navbar>
+          <h1>Kihoot</h1>
+        </>
+      }
+      <Routes>
+        <Route path='/info' element={<Info></Info>}></Route>
+        <Route path='/quiz' element={<Quiz></Quiz>}></Route>
+        <Route path='/profile' element={<Profile></Profile>}></Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem' }}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/quiz">Quiz</Link></li>
+        <li><Link to="/info">Info</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;   
